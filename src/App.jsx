e6ad9aa54416f5dcc5d4aec1fa13bafdff91567c
Kallyne Layane <hellookallyne@gmail.com>
2025-02-23 18:47:30 +0000

    Create the CvCreator component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
+import { CvCreator } from './components/CvCreator';
+
 export const App = () => {
   return (
     <div className="app container">
       <h1 className="sr-only">CV Application</h1>
+
+      <CvCreator>
+        <p>CvCreator</p>
+      </CvCreator>
     </div>
   );
 };
---
+++ src/components/CvCreator/index.jsx
@@ -0,0 +1,5 @@
+import './index.css';
+
+export const CvCreator = ({ children }) => {
+  return <div className="cv-creator">{children}</div>;
+};
